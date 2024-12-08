3ea58e597d69b2fc8b8935bb44e37f7d593242da
cstaubin07 <159050721+cstaubin07@users.noreply.github.com>
2024-12-07 20:16:19 -0600

    Update Model.java

--- samplecode/Model.java
+++ samplecode/Model.java
@@ -1,11 +1,13 @@
-import java.util.*;
-import java.awt.*;
+import java.util.Enumeration;
+import java.util.Vector;
 
 public class Model {
     private Vector<Item> itemList;
+    private Vector<Item> selectedList;
 
     public Model() {
         itemList = new Vector<>();
+        selectedList = new Vector<>();
     }
 
     public void addItem(Item item) {
@@ -18,5 +20,32 @@ public class Model {
 
     public Enumeration<Item> getItems() {
         return itemList.elements();
+    }
+
+    public Enumeration<Item> getSelectedItems() {
+        return selectedList.elements();
+    }
+
+    public void markSelected(Item item) {
+        if (itemList.contains(item)) {
+            itemList.remove(item);
+            selectedList.add(item);
+        }
+    }
+
+    public void unSelect(Item item) {
+        if (selectedList.contains(item)) {
+            selectedList.remove(item);
+            itemList.add(item);
+        }
+    }
+
+    public void deleteSelectedItems() {
+        selectedList.clear();
+    }
+
+    public void setChanged() {
+        // Notify the view to refresh.
+        View.getInstance().refresh();
     }
 }
