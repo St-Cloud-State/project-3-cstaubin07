443e5d05aadb491121c54a2a717e195e052b8d0c
cstaubin07 <159050721+cstaubin07@users.noreply.github.com>
2024-12-07 20:09:41 -0600

    Create Triangle.java

---
+++ samplecode/Triangle.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+
+public class Triangle extends Item {
+    private Point p1, p2, p3;
+
+    public Triangle(Point p1, Point p2, Point p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+    public Point[] getVertices() {
+        return new Point[]{p1, p2, p3};
+    }
+
+    @Override
+    public void render(UIContext context) {
+        Graphics g = context.getGraphics();
+        g.drawLine(p1.x, p1.y, p2.x, p2.y);
+        g.drawLine(p2.x, p2.y, p3.x, p3.y);
+        g.drawLine(p3.x, p3.y, p1.x, p1.y);
+    }
+
+    @Override
+    public void translate(int dx, int dy) {
+        p1.translate(dx, dy);
+        p2.translate(dx, dy);
+        p3.translate(dx, dy);
+    }
+}
