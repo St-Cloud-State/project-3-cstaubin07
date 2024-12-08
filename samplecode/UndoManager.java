ea1fd676faa73e6109f80ba49f968789f37ea2c8
cstaubin07 <159050721+cstaubin07@users.noreply.github.com>
2024-12-07 20:15:42 -0600

    Update UndoManager.java

--- samplecode/UndoManager.java
+++ samplecode/UndoManager.java
@@ -1,9 +1,12 @@
 import java.util.*;
 
+import java.util.*;
+
 public class UndoManager {
     private static Model model;
     private Stack<Command> history;
     private Stack<Command> redoStack;
+    private Command currentCommand;
 
     public UndoManager() {
         history = new Stack<>();
@@ -15,9 +18,34 @@ public class UndoManager {
     }
 
     public void executeCommand(Command command) {
-        command.execute();
-        history.push(command);
-        redoStack.clear();
+        if (command != null) {
+            command.execute();
+            history.push(command);
+            redoStack.clear();
+        }
+    }
+
+    public void beginCommand(Command command) {
+        if (command != null) {
+            command.execute();
+            currentCommand = command;
+        }
+    }
+
+    public void endCommand(Command command) {
+        if (command != null) {
+            if (command == currentCommand) {
+                history.push(command);
+                redoStack.clear();
+                currentCommand = null;
+            }
+        }
+    }
+
+    public void cancelCommand() {
+        if (currentCommand != null) {
+            currentCommand = null;
+        }
     }
 
     public void undo() {
